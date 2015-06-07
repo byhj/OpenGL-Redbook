2149018ef12d3eef53330d60495d94f3698a3c99
binyu <binyuapple@163.com>
2015-06-07 19:17:43 +0800

    Modifed the texture data

--- src/Chapter06/ch06-01-Texture-Data/ch06-01-Texture-Data.cpp
+++ src/Chapter06/ch06-01-Texture-Data/ch06-01-Texture-Data.cpp
@@ -15,24 +15,18 @@ const int Width = 1200, Height = 800;
 
 static const GLfloat VertexData[] = 
 {  
-	 0.75f, -0.75f,  0.0f, 0.0f,
-	-0.75f, -0.75f,	 1.0f, 0.0f,
-	-0.75f,  0.75f,	 1.0f, 1.0f,
-	 0.75f,  0.75f,	 0.0f, 1.0f,
+	0.75f, -0.75f, 
+	-0.75f, -0.75f,	
+	-0.75f,  0.75f,	
+	0.75f,  0.75f,	
+
+	0.0f, 0.0f,
+	1.0f, 0.0f,
+	1.0f, 1.0f,
+	0.0f, 1.0f,
 };	
 static const GLsizei VertexSize = sizeof(VertexData);
 
-static const unsigned char  tex_checkboard_data[] = 
-{
-	0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00,
-	0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF,
-	0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00,
-	0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF,
-	0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00,
-	0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF,
-	0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00,
-	0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF
-};
 
 void init_shader();
 void init_buffer();
@@ -41,11 +35,14 @@ void init_texture();
 
 void init(void)
 {
+	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+	glClearDepth(1.0f);
+
 	init_shader();
 	init_buffer();
 	init_vertexArray();
 	init_texture();
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); //设置颜色缓存为黑色
+	
 }
 
 void reshape(int w, int h)
@@ -57,19 +54,18 @@ void reshape(int w, int h)
 void display(void)
 {
 	//Clear the framebuffer
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glDisable(GL_CULL_FACE);
 
 	//Bind the vao, which manage status we when to render
 	glUseProgram(program);
 	glBindVertexArray(vao);  
+
 	glBindTexture(GL_TEXTURE_2D, tex);
 	glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
 	glutSwapBuffers();
 	glutPostRedisplay();
-
-	glUseProgram(0);
-	glBindVertexArray(0);  
 }
 
 
@@ -88,6 +84,7 @@ int main(int argc,char ** argv)
 
 	init();
 	glutDisplayFunc(display); 
+	glutIdleFunc(display);
 	glutReshapeFunc(reshape); 
 	glutMainLoop();    
 	return 0;
@@ -100,6 +97,7 @@ void init_shader()
 	triangleShader.attach(GL_VERTEX_SHADER, "triangle.vert");
 	triangleShader.attach(GL_FRAGMENT_SHADER, "triangle.frag");
 	triangleShader.link();
+	triangleShader.use();
 	program = triangleShader.GetProgram();
 	tex_loc = glGetUniformLocation(program, "tex");
 	glUniform1i(tex_loc, 0);
@@ -117,31 +115,75 @@ void init_buffer()
 
 void init_vertexArray()
 {
+
+
 	glGenVertexArrays(1, &vao);  
 	glBindVertexArray(vao);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
-	glVertexAttribPointer(1, 2, GL_UNSIGNED_BYTE, GL_TRUE, 4 * sizeof(GLfloat), (GLvoid*)(2 * sizeof(GLfloat) + NULL) );
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FLOAT,0, (GLvoid*)(8 * sizeof(GLfloat) + NULL) );
 
-	glBindVertexArray(0);
-	glDisableVertexAttribArray(0);
-	glDisableVertexAttribArray(1);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
+
+#define B 0x00, 0x00, 0x00, 0x00
+#define W 0xFF, 0xFF, 0xFF, 0xFF
+static const GLubyte tex_data[] =  //棋盘黑白纹理
+{
+	B, W, B, W, B, W, B, W,
+	W, B, W, B, W, B, W, B,
+	B, W, B, W, B, W, B, W,
+	W, B, W, B, W, B, W, B,
+	B, W, B, W, B, W, B, W,
+	W, B, W, B, W, B, W, B,
+	B, W, B, W, B, W, B, W,
+	W, B, W, B, W, B, W, B,
+};
+#undef B
+#undef W
 
 
 void init_texture()
 {
+	/*
+	GLfloat test_tex[] = 
+	{
+		1.0f, 0.0f, 0.0f,  //red
+		0.0f, 1.0f, 0.0f,  //green
+		0.0f, 0.0f, 1.0f,  //blue
+		1.0f, 0.0f, 1.0f,  //pink
+	};
+
+	//内部颜色格式是显卡内格式，即要显示的颜色，在测试出传入rgb分量，显示时如果设置为GL_RG32F，则考虑RG分量
+	glTexStorage2D(GL_TEXTURE_2D, 1, GL_RG32F, 2, 2);
+
+	外部颜色格式是数据传入表示的格式，即规定了数据怎么排列组成颜色分量
+	在test_tex中传入四个颜色值，每三个float组成一个片元的颜色值，对应rgb分量
+	glTexSubImage2D(GL_TEXTURE_2D,
+		0,
+		0, 0,
+		2, 2,
+		GL_RGB, GL_FLOAT,
+	    test_tex);
+	*/
 	glGenTextures(1, &tex);
 	glBindTexture(GL_TEXTURE_2D, tex);
-	glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, 8, 8); //LEVEL must set >=1
-	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 8, 8, GL_RED, GL_UNSIGNED_BYTE, tex_checkboard_data);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA32F , 8, 8);
+
+	glTexSubImage2D(GL_TEXTURE_2D,
+		0,
+		0, 0,
+		8, 8,
+		GL_RGBA, GL_UNSIGNED_BYTE,
+		tex_data);
+
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
--- src/Chapter06/ch06-01-Texture-Data/triangle.vert
+++ src/Chapter06/ch06-01-Texture-Data/triangle.vert
@@ -7,6 +7,6 @@
 
  void main(void)
  {
-    gl_Position =  vec4(Position, 0.0f, 1.0f);
+    gl_Position =  vec4(Position, 0.5f, 1.0f);
     tc = TexCoord;
 }
